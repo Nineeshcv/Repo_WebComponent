5385738a5d7dd0eb77298a209020bd81c76fd5c9
Nineeshcv <nineeshcv@gmail.com>
2020-04-06 10:25:19 +0530

    commiting the card web component

--- web-component2/src/app/card/card.component.ts
+++ web-component2/src/app/card/card.component.ts
@@ -7,15 +7,50 @@ import { Component, OnInit, Input, Injector } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  @Input() imagePath="";
+  @Input() imagePath:string="assets/images/ProfileImage.png";
   @Input() title="John Doe";
   @Input()  text="Some Text";
   @Input()  btnText="Show values";
+  //D:\Repo_WebComponent\Repo_WebComponent\web-component2\src\assets\images\ProfileImage.png
+  
+  @Input() imgFileName:string = "ProfileImage.png";
 
+  public profiles=
+    {
+      "profileTitle":"",
+      "profileText":""
+
+    };
+  
 
   constructor(injector : Injector) { }
 
   ngOnInit(): void {
   }
 
+  formSubmitted = false;
+  onSubmit() {
+     // this.title = this.profiles.profileTitle;
+     // this.text = this.profiles.profileText;
+      this.onAdd(this.profiles.profileTitle,this.profiles.profileText);
+
+     this.formSubmitted = true;
+  }
+  public proArray:any=[];
+  
+  onAdd(t:string,m:string) {
+    this.proArray.push(new Profile(t,m));
+    console.log(this.proArray);
+  }
+
+  onRemove(){
+    //this.proArray.pop();
+  }
+
+}
+
+export class Profile {
+  constructor(
+    public profileTitle: string,
+    public profileMsg: string) { }
 }
